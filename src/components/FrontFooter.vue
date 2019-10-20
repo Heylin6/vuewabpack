dd20dfa4aa336b6b4e6d933c9be16b45808030ab
heylin6 <ZO5147yexxo0@gmail.com>
2019-10-20 16:55:18 +0800

    前台footer新增github路徑

--- src/components/FrontFooter.vue
+++ src/components/FrontFooter.vue
@@ -2,7 +2,7 @@
     <div>
          <footer class="container">
           <p class="float-right"><a href="#">Back to top</a></p>
-          <p>&copy; 2017-2019 Company, Inc. &middot; <a href="#">Privacy</a> 
+          <p>&copy; 2017-2019 Company, Inc. 
           &middot; 
             <a href="#">
                <router-link to="/login" >                     
@@ -11,6 +11,11 @@
                </router-link>
               
             </a>
+          &middot; 
+          <a href="https://github.com/Heylin6/vuewabpack" 
+              target="_blank">
+              github原始碼
+          </a>
           </p>
         </footer>
     </div>
